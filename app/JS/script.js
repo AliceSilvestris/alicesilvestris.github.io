596a544b10ab41fb9f711f43594e1c74c8a152ad
AliceSilvestris <143886206+AliceSilvestris@users.noreply.github.com>
2023-09-03 19:01:19 +0100

    dsad

---
+++ app/JS/script.js
@@ -0,0 +1,30 @@
+import "bootstrap";
+import "Style";
+const btnhamburger = document.querySelector('#btnhamburger');
+const body = document.querySelector('body')
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems= document.querySelectorAll('.has-fade');
+
+btnhamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(header.classList.contains('open')){//menu closed
+        header.classList.remove('open');
+        body.classList.add('noscroll');
+        fadeElems.forEach(function(element){
+         element.classList.remove('fade-in');
+         element.classList.add('fade-out');
+        });
+    }
+    else{                                 //menue opened
+        header.classList.add('open');
+        body.classList.remove('noscroll');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
+
+    }
+    
+});
